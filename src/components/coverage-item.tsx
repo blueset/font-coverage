78173d8fa4937d1ef3e34b3e55529d2ada1f1b20
Eana Hufwe <eana@1a23.com>
2026-01-11 21:01:03 -0800

    More style update

--- src/components/coverage-item.tsx
+++ src/components/coverage-item.tsx
@@ -1,13 +1,12 @@
 import {
   Item,
-  ItemActions,
   ItemContent,
   ItemDescription,
   ItemTitle,
 } from "@/components/ui/item";
 import { MissingCodepoints, MissingGlyphs } from "./missing-dialog";
 import { cn } from "@/lib/utils";
-import { Check } from "lucide-react";
+import { CircleCheckBig } from "lucide-react";
 
 export function CoverageItem({
   name,
@@ -27,40 +26,35 @@ export function CoverageItem({
   includedCodepoints?: number[];
 }) {
   const item = (
-    <Item variant="outline" className="p-3">
-      <ItemActions>
-        <div
-          className={cn(
-            "bg-muted rounded-full size-10",
-            coverage === total && "bg-green-700 dark:bg-green-300"
-          )}
-        >
-          {coverage === total ? (
-            <Check className="m-2.5 size-5 text-green-200 dark:text-green-900" />
-          ) : (
-            <div
-              className="rounded-full size-full"
-              style={{
-                backgroundImage: `conic-gradient(var(--foreground) 0% ${
-                  (coverage / total) * 100
-                }%, var(--muted) ${(coverage / total) * 100}% 100%)`,
-              }}
-            >
-              <div className="place-content-center size-full font-medium text-white text-center mix-blend-difference">
-                {Math.max(1, Math.min((coverage / total) * 100, 99)).toFixed(0)}
-                <small>%</small>
-              </div>
-            </div>
-          )}
-        </div>
-      </ItemActions>
+    <Item variant="outline" className="relative p-3 overflow-clip">
+      <div
+        className="absolute inset-0 bg-linear-to-r from-transparent to-green-700/10 dark:to-green-300/10 border-r border-r-green-700/40 dark:border-r-green-300/40"
+        style={{
+          translate: `${-(1 - coverage / total) * 100}% 0`,
+        }}
+      ></div>
       <ItemContent>
         <ItemTitle className="flex justify-stretch items-center gap-y-0 w-full min-h-8">
-          <span className="grow">{name}</span>
-          <ItemDescription className="shrink-0">
-            <span>
+          <span className={cn("grow")}>
+            {coverage === total && (
+              <CircleCheckBig className="inline mr-1.5 size-4 text-green-600 dark:text-green-300 -translate-y-0.25" />
+            )}
+            {name}
+          </span>
+          <ItemDescription className="flex flex-col items-end shrink-0">
+            <div
+              style={
+                {
+                  "--percentage": `${(coverage / total) * 100}%`,
+                } as React.CSSProperties
+              }
+              className="font-medium text-[color-mix(in_srgb,var(--color-green-600)_var(--percentage),var(--color-muted-foreground))] dark:text-[color-mix(in_srgb,var(--color-green-300)_var(--percentage),var(--color-muted-foreground))]"
+            >
+              {Math.floor((coverage / total) * 100).toFixed(0)}%
+            </div>
+            <small>
               {coverage} / {total}
-            </span>
+            </small>
           </ItemDescription>
         </ItemTitle>
       </ItemContent>
